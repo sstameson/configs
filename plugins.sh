882e99fc728a4ca8ed274cb2b86ba272a40bb9c0
Spiro Stameson <sstameso@caltech.edu>
2022-12-16 10:21:01 -0800

    Update installation

---
+++ plugins.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.vim/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+git clone --depth 1 https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/git-plugins/start/ctrlp
